56ab3b340e3aeb9df6328bc25e6cdf211a1b4185
Jeremiah <jeremiahmarcos27@outlook.com>
2022-01-13 14:20:39 -0600

    created some of the form template

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
-
+import LoginFrom from "./components/LoginForm"
+import SignupForm from "./components/SignupForm"
 function App() {
   return (
     <div className="App">
       <h1 className="testing">HELLO</h1>
+      <LoginFrom />
+      <SignupForm />
     </div>
   );
 }
--- db/migrate/20220113160112_add_admin_column_to_user.rb
+++ db/migrate/20220113160112_add_admin_column_to_user.rb
@@ -3,3 +3,8 @@ class AddAdminColumnToUser < ActiveRecord::Migration[6.1]
     add_column :users, :admin, :boolean, default: false
   end
 end
+
+
+# user has one user_roster
+# user_roster belongs to user and has_many players
+# players has_many user_roster
